92a00c6d6204c3cb5f55c2b50d821b6ee8bb523b
jordles113 <jordan.t.romero@gmail.com>
2021-03-22 17:06:45 -0500

    writes a promise

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('I could not find that file');
+      resolve(data);
+    });
+  });
+};
+
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   console.log(`Breed: ${data}`);
 
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
+    .then((res) => {
       console.log(res.body.message);
 
       fs.writeFile('dog-img.txt', res.body.message, (err) => {
         console.log('Random Dog Image Saved to File');
       });
+    })
+    .catch((err) => {
+      console.log(err.message);
     });
 });
